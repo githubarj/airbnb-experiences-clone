496b4a7830ebd73273bcc9024586cb6dec335c44
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-12 22:25:11 +0300

    reusable props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from "react"
-import "./index.css"
-import Navbar from "./Navbar"
-import Hero from "./Hero"
-import Card from "./Card"
+import React from "react";
+import "./index.css";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Card from "./Card";
+import katie from "./assets/katie-zaferes.png";
 
-export default function App () {
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
+export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card />
+
+      <Card
+        img={katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lesons with Katie Zaferes"
+        price={136}
+      />
     </div>
-  )
+  );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import katie from "./assets/katie-zaferes.png"
 import star from "./assets/star.png"
 
-export default function Card () {
+export default function Card ({img, rating, reviewCount, country, title, price}) {
     return (
         <div className="card">
-            <img src={katie} alt="katie" className="katie-pic"></img>
+            <img src={img} alt="katie" className="katie-pic"></img>
             <section>
-                <p className="light"><img src={star} alt="star" className="star"></img>5.0(6).USA</p>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><strong>From $136</strong> / person</p>
+                <p className="light"><img src={star} alt="star" className="star"></img> {rating} <span>({reviewCount}) •{country}</span></p>
+                <p>{title}</p>
+                <p><strong>From {price}</strong> / person</p>
             </section>
         </div>
     )

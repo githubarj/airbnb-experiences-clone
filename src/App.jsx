d08e2dc607c0d6c19a80a2a6b1f5a1665172fa7a
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-12 09:46:45 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import "./index.css"
 import Navbar from "./Navbar"
+import Hero from "./Hero"
 
 export default function App () {
   return (
-    <div>
+    <div className="container">
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css"
+import photoGrid from "./assets/photo-grid.png"
+
+export default function Hero () {
+    return (
+        <div className="hero">
+            <div className="photo-section">
+                <img src={photoGrid} alt="photo grid" className="photo-grid"></img>
+            </div>
+            <h1>Online experiences</h1>
+            <p>Join unique interactive experiences led by one-of-a kind hosts-all without leaving home</p>
+        </div>
+    )
+}
